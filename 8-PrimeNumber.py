a483da977e77a771a10ac2f30bfff9ae5d4dbfff
Sadra <123418962+SadraGt@users.noreply.github.com>
2025-01-29 14:43:01 +0330

    Update 8-PrimeNumber.py
    Optimization

--- 8-PrimeNumber.py
+++ 8-PrimeNumber.py
@@ -1,12 +1,10 @@
 number = int(input("Enter Number :"))
-Prime = True
+
 for i in range (2,number):
     x = number % i 
     if x == 0:
         print(f"{number} is not prime! ")
-        Prime = False
         break
-
-if Prime == True:
-    print(f"{number} is a prime number ")
+    else:
+        print(f"{number} is a prime number ")
         
